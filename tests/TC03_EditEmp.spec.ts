90a9e46885ac477282f15e655e4ab10b48ffc8d0
Chennappa1408 <chennappa.infy@gmail.com>
2026-06-14 19:01:27 +0530

    Update TC03_EditEmp.spec.ts
    Adding more loggers

--- tests/TC03_EditEmp.spec.ts
+++ tests/TC03_EditEmp.spec.ts
@@ -14,4 +14,5 @@ test('TC03_EditEmp', async ({ page }) => {
     console.log("Logout is successful resring purpose");
     await gen.waitForSomeTime(3000);
     //this is the last statement
+    console.log("Demo is successful for GIT Operations purpose");
 });
